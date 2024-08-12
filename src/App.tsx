508eea4c66dfd5b7ce82971220baab2a848e5cf2
Mateusz Sznura <mateusz.sznura@ulam.io>
2024-08-12 15:32:15 +0200

    add Asset interface and cleanup unused file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
+type Asset = {
+  chain_id: string;
+  denom: string;
+  id: string;
+  logo_uri: string;
+  name: string;
+  symbol: string;
+};
+
 function App() {
   return <div>hello</div>;
 }
--- src/get-id.ts
+++
@@ -1,1 +0,0 @@
-export const getId = (asset: any) => `${asset.chain_id}-${asset.denom}`;
